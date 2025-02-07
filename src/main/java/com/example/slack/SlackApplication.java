9d9a7cd09f82e55938031db2e786daad29ab49f1
dkfdj <dkfdj01033@gmail.com>
2025-02-07 23:47:36 +0900

    initial commit

--- src/main/java/com/example/slack/SlackApplication.java
+++ src/main/java/com/example/slack/SlackApplication.java
@@ -2,12 +2,14 @@ package com.example.slack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SlackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SlackApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SlackApplication.class, args);
+        SpringApplication.exit(run);
     }
 
 }
